272d7d99eee22a2ab16ff3d4659de22ddd5f8198
albert6051 <albert.chen6051@gmail.com>
2019-05-20 01:13:00 -0400

    Update for wait and broadcast

--- user.rkt
+++ user.rkt
@@ -4,65 +4,178 @@
 
 (define rest cdr)
 
-(define th-list '())
+(define get-signal
+  (lambda (entry)
+    (car (car entry))))
 
-(define th-yield
+(define get-wait-list
+  (lambda (entry)
+    (car (cdr entry))))
+
+(define terminate 'terminate)
+
+; This the processes schedule quene, FIFO base
+(define thread-quene '())
+
+; This the table for all the processes that are waiting for a signal to move to schedule quene
+(define wait-table '())
+
+; Set terminate point when there is no next thread
+(define set-end
   (lambda ()
-    (if(null? th-list)
+    (let (
+      (cont (call/cc (lambda (c) c))))
+      (if (procedure? cont)
+          (let* (
+                 (next-thread (first thread-quene))
+                 (remaining-thread (rest thread-quene)))
+            (set! terminate cont)
+            (set! thread-quene remaining-thread)
+            (next-thread 'next))
+          (display 'terminate)))))
+   
+
+; This function will save the current stack of the process and put it at the end of the schedule quene
+(define thread-yield
+  (lambda ()
+    (if(null? thread-quene)
        (display '(no thread in the list no need to wait))
        (let (
              (cont (call/cc (lambda (c) c))))
          (if (procedure? cont)
              (let* (
-                    (next-th (first th-list))
-                    (remaining-th (rest th-list))
-                    (new-th-ls
-                     (append remaining-th (list cont))))
-               (set! th-list new-th-ls)
-               (next-th 'next))
-             (display '(end yield)))))))
+                    (next-thread (first thread-quene))
+                    (remaining-thread (rest thread-quene))
+                    (new-thread-quene
+                     (append remaining-thread (list cont))))
+               (set! thread-quene new-thread-quene)
+               (next-thread 'next))
+             (display '(thread continue)))))))
 
-(define th-end
+; This function generate a table that with the new entry of signal and its process
+(define new-wait-table
+  (lambda (signal cont table)
+    (cond ((null? table)
+           (list (append table (list (list signal) (list cont)))))
+          ((eq? signal (get-signal(first table)))
+           (cons (list
+                  (list signal)
+                  (append (get-wait-list (first table)) (list cont)))
+                 (rest table)))
+          (else (cons (first table) (new-wait-table signal cont (cdr table)))))))
+
+; This function save the process stack and put it on wait table, then call the next thread
+(define wait-signal
+  (lambda (signal)
+    (if(null? thread-quene)
+       (terminate 'end)
+       (let (
+             (cont (call/cc (lambda (c) c))))
+         (if (procedure? cont)
+             (let* (
+                    (next-thread (first thread-quene))
+                    (remaining-thread (rest thread-quene))
+                    (new-table
+                     (new-wait-table signal cont wait-table)))
+               (set! wait-table new-table)
+               (set! thread-quene remaining-thread)
+               (next-thread 'next))
+             (display '(thread continue)))))))
+
+; This function release the process that receive the signal to schedule quene
+(define add-proc-list
+  (lambda (proc-list)
+    (let (
+          (new-thread-quene (append thread-quene proc-list)))
+      (set! thread-quene new-thread-quene))))
+
+; This function generate a wait table that remove entry from wait table with giving signal
+(define remove-signal-table
+  (lambda (signal table)
+    (cond ((null? table) table)
+          ((eq? signal (get-signal(first table)))
+           (cdr table))
+          (else (cons (first table) (remove-signal-table signal (rest table)))))))
+
+; This function 
+(define broadcast
+  (lambda (signal table)
+    (cond ((null? table) #t)
+          ((eq? signal (get-signal(first table)))
+           (add-proc-list (get-wait-list(first table)))
+           (set! wait-table (remove-signal-table signal wait-table)))
+          (else (broadcast signal (rest table))))))
+
+; This function will tell the process is end and call the next process on the schedule quene
+; or exit if there is no more process in the quene
+(define thread-end
   (lambda ()
-    (if (null? th-list)
+    (if (null? thread-quene)
         '(exit)
         (let* (
-               (next-th (first th-list))
-               (remaining-th (rest th-list)))
-          (set! th-list remaining-th)
-          (next-th 'next)))))
+               (next-thread (first thread-quene))
+               (remaining-thread (rest thread-quene)))
+          (set! thread-quene remaining-thread)
+          (next-thread 'next)))))
 
-(define th-new
-  (lambda (th-proc)
+; This function will create thread for the process and save stack of the process using continuaton
+; which allow the process to yield and call the next thread
+(define create-thread
+  (lambda (thread-process)
     (let (
           (cont (call/cc (lambda (c) c))))
       (if (procedure? cont)
           (begin
-            (set! th-list (append th-list (list cont)))
-            (th-proc)
-            (display '(continue thread))
-            (th-end))
+            (set! thread-quene (append thread-quene (list cont)))
+            (thread-process)
+            (thread-end))
           'end))))
 
+
+; Testing
+;(define proc-1
+  ;(lambda ()
+    ;(display "hello 1\n")
+    ;(thread-yield)
+    ;(display "hello 1 again\n")))
+;(define proc-2
+  ;(lambda ()
+    ;(display "hello 2\n")
+    ;(thread-yield)
+    ;(display "hello 2 again\n")
+    ;(thread-yield)
+    ;(display "hello 2 again again\n")))
+;(define proc-3
+  ;(lambda ()
+    ;(display "hello 3\n")
+    ;(thread-yield)
+    ;(display "hello 3 again\n")))
 (define proc-1
   (lambda ()
-    (display "hello 1\n")
-    (th-yield)
-    (display "hello 1 again\n")))
+    (display "proc-1 wait for signal first\n")
+    (wait-signal 'first)
+    (display "proc-1 receive signal first\n")
+    (display "proc-1 wait for signal second\n")
+    (wait-signal 'second)
+    (display "proc-1 receive signal second\n")))
 (define proc-2
   (lambda ()
-    (display "hello 2\n")
-    (th-yield)
-    (display "hello 2 again\n")
-    (th-yield)
-    (display "hello 2 again again\n")))
+    (display "proc-2 wait for signal second\n")
+    (wait-signal 'second)
+    (display "proc-2 receive signal second\n")))
 (define proc-3
   (lambda ()
-    (display "hello 3\n")
-    (th-yield)
-    (display "hello 3 again\n")))
+    (display "proc-3 broadcast signal first\n")
+    (broadcast 'first wait-table)))
+(define proc-4
+  (lambda ()
+    (display "proc-4 broadcast signal second\n")
+    (broadcast 'second wait-table)))
 
-(th-new proc-1)
-(th-new proc-2)
-(th-new proc-3)
-(th-end)
+
+(create-thread proc-1)
+(create-thread proc-2)
+(create-thread proc-3)
+(create-thread proc-4)
+(set-end)
+(thread-end)
